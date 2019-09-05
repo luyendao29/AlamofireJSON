2c298354a20782886671e5507f82b5bab69818a2
Boss <luyenddgc00774@gmail.com>
2019-09-05 15:41:39 +0700

    Alamofire...

---
+++ AlamofireJSON/AlamofireJSON/Extension.swift
@@ -0,0 +1,9 @@
+//
+//  Extension.swift
+//  AlamofireJSON
+//
+//  Created by Boss on 9/5/19.
+//  Copyright © 2019 LuyệnĐào. All rights reserved.
+//
+
+import UIKit
---
+++ AlamofireJSON/AlamofireJSON/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlamofireJSON
+//
+//  Created by Boss on 9/5/19.
+//  Copyright © 2019 LuyệnĐào. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
